c38fd7f69256fe6d32199c354cd816c7f74ece8a
202202608 오태영 <otaeyeong2003@gmail.com>
2025-11-11 14:37:14 +0900

    과제 제출

--- game.cpp
+++ game.cpp
@@ -21,6 +21,7 @@ string Bow::getDescription() const {
     return character->getDescription() + ", Bow";
 }
 
+int Armor::getAttack() const { return character->getAttack();}
 int Armor::getDefense() const {
     return character->getDefense() + 30;
 }
@@ -28,6 +29,8 @@ int Armor::getSpeed() const {
     return character->getSpeed() - 5;
 }
 
+int Boots::getAttack() const { return character->getAttack();}
+int Boots::getDefense() const {return character->getDefense();}
 int Boots::getSpeed() const {
     return character->getSpeed() + 15;
 }
@@ -36,16 +39,23 @@ int Staff::getAttack() const {
     //if (description.compare("Wizard") == 0) return character->getAttack()+60;
     return character->getAttack()+20;
 }
+int Staff::getDefense() const {return character->getDefense();}
+int Staff::getSpeed() const {return character->getSpeed();}
 
 int Bow::getAttack() const {
     //if (description.compare("Archer") == 0) return character->getAttack()+65;
     return character->getAttack()+25;
 }
+int Bow::getDefense() const {return character->getDefense();}
+int Bow::getSpeed() const {return character->getSpeed();}
+
 
 int Sword::getAttack() const {
     //if (description.compare("Knight") == 0) return character->getAttack()+70;
     return character->getAttack()+30;
 }
+int Sword::getDefense() const {return character->getDefense();}
+int Sword::getSpeed() const {return character->getSpeed();}
 
 
 
